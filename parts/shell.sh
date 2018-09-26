c17b12ae9b1bbadb5b808c5b97d6661570def583
Fabian Sorqvist <fabian@sorqvist.nu>
2018-09-26 16:18:08 +0200

    install tmux

--- parts/shell.sh
+++ parts/shell.sh
@@ -4,6 +4,7 @@
 # you can tell your mom about on the next family dinner.
 
 brew cask install iterm2
+brew install tmux
 
 if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
